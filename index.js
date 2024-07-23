448e479d5bd12ea2afe6c4839d9977a8d7be363e
sniffjoke <sniffjoke@gmail.com>
2024-07-23 17:37:05 +0200

    array methods push, pop, shift, unshift

---
+++ index.js
@@ -0,0 +1,41 @@
+//методы массивовю Полифилы
+//push, pop, shift, unshift - мутабельные методы массива
+//push - добавляет 1 и более элементов в конец массива
+
+// const names = ['Alex', 'Bob', 'Ivan', 'Sasha'];
+// const pushResult = names.push('1', {car: 'bmw'}, 3) // - возвращает длину массива
+//
+// console.log('names: ', names)
+// console.log('pushResult: ', pushResult)
+
+//pop - удаляет последний элемент массива
+
+// const names2 = ['Alex', 'Bob', 'Ivan', 'Sasha']
+// const names2Result = names2.pop()
+//
+// const arr1 = []
+// const resultArr1 = arr1.pop()
+//
+// console.log(names2)
+// console.log('names2Result: ', names2Result) // - возвращает удаленный элемент
+// console.log('resultArr1: ', resultArr1)
+
+// shift - удаляет первый элемент массива
+
+// const names3 = ['Alex', 'Bob', 'Ivan', 'Sasha']
+// const resultNames3 = names3.shift()
+//
+// const arr2 = []
+// const resArr2 = arr2.shift()
+//
+// console.log('names3: ', names3)
+// console.log('resultNames3: ', resultNames3) // - возвращает удаленный элемент
+// console.log('resArr2: ', resArr2)
+
+// unshift - добавляет 1 и более элементов в начало массива
+
+// const names4 = ['Alex', 'Bob', 'Ivan', 'Sasha']
+// const unshiftResult = names4.unshift('test value1', ['nre value 1', 'new value 2'], 3)
+//
+// console.log('names4: ', names4)
+// console.log('unshiftResult: ', unshiftResult) // - возвращает длину массива
